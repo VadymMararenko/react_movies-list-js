38e393edb551a458ed02366e4e06c7342d8a3371
Vadym Mararenko <mararenkovadim@gmail.com>
2026-08-27 12:46:35 +0300

    fix movielist

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,12 @@
 import './MovieList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MovieList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard movie={movie} key={movie.imdbId} />
-    ))}
-  </div>
-);
+export const MovieList = ({ movies }) => {
+  return (
+    <div className="movies">
+      {movies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
